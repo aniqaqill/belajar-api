f3309c6ea7b6ea0b710096bf885da87bcb2d1460
Muhammad Aniq Aqil Bin Azrai Fahmi <aniqaqil06@gmail.com>
2023-03-13 23:54:07 +0800

    deployment

---
+++ build/precache-manifest.e13e59f54b5d06ae8d5f13e2fb15d89c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e4939987497738ffa2f3b8a8b4cc121",
+    "url": "/belajar-api/index.html"
+  },
+  {
+    "revision": "f2c271098657e95737e5",
+    "url": "/belajar-api/static/css/main.144eb542.chunk.css"
+  },
+  {
+    "revision": "cf145d0b7e900af7d821",
+    "url": "/belajar-api/static/js/2.5fcc2b98.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/belajar-api/static/js/2.5fcc2b98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2c271098657e95737e5",
+    "url": "/belajar-api/static/js/main.6fb80fbb.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/belajar-api/static/js/main.6fb80fbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4a10ebe7ce1cfeb1b65",
+    "url": "/belajar-api/static/js/runtime-main.8315a828.js"
+  }
+]);
